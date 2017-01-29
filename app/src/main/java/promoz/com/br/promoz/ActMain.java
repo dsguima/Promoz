c20efcf2ca6269706cb25c18f0c17aa9aab32410
RafaelNeveZ <rafaelnevesgonzaga@gmail.com>
2017-01-29 20:39:47 -0200

    Designe do Navigation Drawer atualizado. Menu do lado direito deletado. Carteira Activty criada (Não modificada).

--- app/src/main/java/promoz/com/br/promoz/ActMain.java
+++ app/src/main/java/promoz/com/br/promoz/ActMain.java
@@ -1,5 +1,6 @@
 package promoz.com.br.promoz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import promoz.com.br.promoz.dao.db.MySQLiteDatabase;
 
@@ -55,34 +57,15 @@ public class ActMain extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.act_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+    /*
         if (id == R.id.nav_camera) {
             Log.v("TESTE","Chama Activity");
             Intent intent = new Intent(this, ActTeste.class);
@@ -98,6 +81,60 @@ public class ActMain extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        }*/
+        if (id == R.id.nav_perfil) {
+            Context contexto = getApplicationContext();
+            String texto = "PERFIL";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_wallet) {
+            Intent i = new Intent(this,CarteiraActivity.class);
+            this.startActivity(i);
+
+        } else if (id == R.id.nav_missions) {
+            Context contexto = getApplicationContext();
+            String texto = "MISSÔES";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_shop) {
+            Context contexto = getApplicationContext();
+            String texto = "LOJA";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_config) {
+            Context contexto = getApplicationContext();
+            String texto = "CONFIGURAÇÕES";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_help) {
+            Context contexto = getApplicationContext();
+            String texto = "AJUDA";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_feedback) {
+            Context contexto = getApplicationContext();
+            String texto = "FEEDBACK";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+        } else if (id == R.id.nav_terms) {
+            Context contexto = getApplicationContext();
+            String texto = "TERMOS";
+            int duracao = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
 
         }
 
---
+++ app/src/main/java/promoz/com/br/promoz/CarteiraActivity.java
@@ -0,0 +1,13 @@
+package promoz.com.br.promoz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarteiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carteira);
+    }
+}
