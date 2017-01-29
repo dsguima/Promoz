5548f8bcfe31523856eaf20b636d1f4e7ec1d054
RafaelNeveZ <rafaelnevesgonzaga@gmail.com>
2017-01-29 20:57:19 -0200

    Update no deawable Tela Carteira atualizada com as tabs.

--- app/src/main/java/promoz/com/br/promoz/CarteiraActivity.java
+++ app/src/main/java/promoz/com/br/promoz/CarteiraActivity.java
@@ -1,7 +1,11 @@
 package promoz.com.br.promoz;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class CarteiraActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class CarteiraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carteira);
+
+        //Back button on header
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                CarteiraActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
