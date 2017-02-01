a90e4f20557de88f207228c1597dd4e16d8c842d
dsguima <danitgb@gmail.com>
2017-02-01 20:37:00 -0300

    Merge branch 'master' of /Users/dsguima/Desktop/DevAndroid/Projetos/Promoz with conflicts.

--- app/src/main/java/promoz/com/br/promoz/ActMain.java
+++ app/src/main/java/promoz/com/br/promoz/ActMain.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +11,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class ActMain extends AppCompatActivity
